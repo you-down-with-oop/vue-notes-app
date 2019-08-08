a1534e4c0a26b4f9bc6546fc6ff785d4cf9d22e0
Peter Jang <peter.x.jang@gmail.com>
2019-08-08 18:37:48 -0500

    Move note container into separate component

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,19 @@
 <template>
   <div id="app">
     <toolbar />
-    <div class="note-container">
-      <div class="note-selectors">
-        <div class="note-selector active">
-          <p class="note-selector-title">First note...</p>
-          <p class="note-selector-timestamp">Timestamp here...</p>
-        </div>
-        <div class="note-selector">
-          <p class="note-selector-title">Second note...</p>
-          <p class="note-selector-timestamp">Timestamp here...</p>
-        </div>
-        <div class="note-selector">
-          <p class="note-selector-title">Third note...</p>
-          <p class="note-selector-timestamp">Timestamp here...</p>
-        </div>
-      </div>
-      <div class="note-editor">
-        <p class="note-editor-info">Timestamp here...</p>
-        <textarea class="note-editor-input">
-         First note...
-        
-         Note text here...
-       </textarea
-        >
-      </div>
-    </div>
+    <note-container />
   </div>
 </template>
 
 <script>
 import Toolbar from "./components/Toolbar";
+import NoteContainer from "./components/NoteContainer";
 
 export default {
   name: "app",
   components: {
-    Toolbar
+    Toolbar,
+    NoteContainer
   }
 };
 </script>
---
+++ src/components/NoteContainer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="note-container">
+    <div class="note-selectors">
+      <div class="note-selector active">
+        <p class="note-selector-title">First note...</p>
+        <p class="note-selector-timestamp">Timestamp here...</p>
+      </div>
+      <div class="note-selector">
+        <p class="note-selector-title">Second note...</p>
+        <p class="note-selector-timestamp">Timestamp here...</p>
+      </div>
+      <div class="note-selector">
+        <p class="note-selector-title">Third note...</p>
+        <p class="note-selector-timestamp">Timestamp here...</p>
+      </div>
+    </div>
+    <div class="note-editor">
+      <p class="note-editor-info">Timestamp here...</p>
+      <textarea class="note-editor-input">
+         First note...
+        
+         Note text here...
+       </textarea
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "note-container"
+};
+</script>
