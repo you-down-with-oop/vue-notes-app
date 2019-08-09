dc34f56148f4f6f65557f73b651e5d463e7776e1
Peter Jang <peter.x.jang@gmail.com>
2019-08-08 19:25:40 -0500

    Store notes in data and generate each note selector component with v-for

--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="note-selectors">
-    <note-selector />
-    <note-selector />
-    <note-selector />
+    <note-selector v-for="note in notes" v-bind:note="note" v-bind:key="note.id" />
   </div>
 </template>
 
@@ -13,6 +11,16 @@ export default {
   name: "note-selectors",
   components: {
     NoteSelector
+  },
+  data: function() {
+    return {
+      notes: [
+        { id: 1, body: "This is a first test", timestamp: Date.now() },
+        { id: 2, body: "This is a second test", timestamp: Date.now() },
+        { id: 3, body: "This is a third test", timestamp: Date.now() },
+        { id: 4, body: "This is a fourth test", timestamp: Date.now() }
+      ]
+    };
   }
 };
 </script>
